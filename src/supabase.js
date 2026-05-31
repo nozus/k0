47868db87aed583114eb7f4755db445a73de809b
spencer <spencerkirklandd@gmail.com>
2026-05-31 11:56:50 -0700

    fix: correct typo in supabase url

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const SUPABASE_URL = 'https://xmjpoopbrbcrviwbzkul.supabase.co';
+const SUPABASE_URL = 'https://xmjpoozbrbcrviwbzkul.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhtanBvb3picmJjcnZpd2J6a3VsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAyMzc3MzEsImV4cCI6MjA5NTgxMzczMX0.58c4OiAG5AfutdckkEKdZaGvI3ltoFRTW6vP_QtvYq8';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
